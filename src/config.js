a06496a7a3bd0d7768f9ef261b80c91c1b367555
timothyAgevi <timothyagevi@gmail.com>
2021-06-25 19:14:04 +0300

    serverURL

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const serverURL = "https://pdf-convertor-server.herokuapp.com";
+// export const serverURL = "https://pdf-convertor-server.herokuapp.com";
+export const serverURL = "https://condescending-brattain-01587c.netlify.app"
